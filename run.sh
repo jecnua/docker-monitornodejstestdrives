0e705125099f1ac2f9bdae22b126b186f19bf5c5
Fabrizio Sabatini <fabrizio.sabatini.it@gmail.com>
2018-04-23 17:56:57 +0100

    Fixing bash lint

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ then
   exit 1
 fi
 
-if [ -z ${NAME_REPO+x} ];
+if [ -z "${NAME_REPO+x}" ];
 then
   echo "NAME_REPO is unset";
   NAME_REPO=$(basename "$(git rev-parse --show-toplevel)" | sed 's/-/\//g')
