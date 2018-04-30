5f8c30d26ac7844ea683280d66973074a3e9e47b
Fabrizio Sabatini <fabrizio.sabatini.it@gmail.com>
2018-04-30 14:56:10 +0100

    Added make and env in both POC.

---
+++ elasticsearch/env.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export DOCKER_IMAGE=jecnua/monitor-nodejs-elasticapm
+export URL_REPO=jecnua/monitor-nodejs-elasticapm
---
+++ prometheus/env.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export DOCKER_IMAGE=jecnua/monitor-nodejs-prom
+export URL_REPO=jecnua/monitor-nodejs-prom
